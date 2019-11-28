0473e26a7f0639edb39774663aa454a3cc32479e
xiongpu <18575696574@163.com>
2019-11-28 20:30:22 +0800

    Fix #8 支持注解方式使用SIMPLE模式的调度

---
+++ chronus-sdk/chronus-sdk-api/src/main/java/com/qihoo/finance/chronus/sdk/annotation/Job.java
@@ -0,0 +1,16 @@
+package com.qihoo.finance.chronus.sdk.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface Job {
+    /**
+     * Job定义的id 格式为 所在Bean名字.系统内唯一标识 如: XXXJob.deleteLog
+     * @return
+     */
+    String key();
+}
